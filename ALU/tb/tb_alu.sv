2aad84f8000977293890564f4faa0a53291914d5
Brandon Wang <brandonwangHSV@gmail.com>
2026-01-06 17:11:49 -0800

    Wrote own randomize function, bug fixing

--- ALU/tb/tb_alu.sv
+++ ALU/tb/tb_alu.sv
@@ -4,15 +4,18 @@
 // Import opcode enum and alu module
 `include "alu.sv"
 
+// DEFINE ALU WIDTH HERE
+`define ALU_WIDTH 8
+
 /*
 * Transactions specify one "round" of stimuli (inputs) to the DUT.
 * Transactions also store DUT outputs for correctness checking.
 */
 class alu_transaction #(int WIDTH = 8);
-    rand logic[WIDTH-1:0] in_A;  // First input
-    rand logic[WIDTH-1:0] in_B;  // Second input
-    rand logic            c_in;  // Carry in
-    rand opcode           op;    // Operation
+    logic[WIDTH-1:0] in_A;  // First input
+    logic[WIDTH-1:0] in_B;  // Second input
+    logic            c_in;  // Carry in
+    opcode           op;    // Operation
 
     logic[WIDTH-1:0] out;        // Output
     logic            c_out;      // Carry out
@@ -23,6 +26,14 @@ class alu_transaction #(int WIDTH = 8);
 
     function new(); endfunction
 
+    // Write my own randomize function cuz I don't have a license :/
+    function void new_random_inputs();
+        in_A = (WIDTH-1)'($urandom);
+        in_B = (WIDTH-1)'($urandom);
+        c_in = (WIDTH-1)'($urandom);
+        op = opcode'($urandom);
+    endfunction
+
     // inputs_to_string() formats the inputs to the DUT
     function string inputs_to_string();
         return $sformatf("A=%0d B=%0d c_in=%1b opcode=%4b", in_A, in_B, c_in, op);
@@ -30,9 +41,8 @@ class alu_transaction #(int WIDTH = 8);
 
     // result_to_string() formats the full transaction contents including inputs / outputs
     function string result_to_string();
-        return $sformatf("A=%0d B=%0d c_in=%1b opcode=%4b out=%0d c_out=%1b 
-        f_zero=%1b f_negative=%1b f_overflow=%1b f_parity=%1b",
-        in_A, in_B, c_in, opcode, out, c_out, f_zero, f_negative, f_overflow, f_parity);
+        return $sformatf("A=%0d B=%0d c_in=%1b opcode=%4b out=%0d c_out=%1b f_zero=%1b f_negative=%1b f_overflow=%1b f_parity=%1b",
+            in_A, in_B, c_in, op, out, c_out, f_zero, f_negative, f_overflow, f_parity);
     endfunction
 endclass
 
@@ -45,8 +55,6 @@ endclass
 * Parameterize the width of the ALU by changing ALU_WIDTH below.
 */
 module tb_alu;
-    int ALU_WIDTH = 8; // Bit width of DUT
-
     int num_correct; // Number of correct transactions so far
     int num_total; // Number of total transactions so far
     int verbose = 0; // Whether to print verbose logs
@@ -54,10 +62,14 @@ module tb_alu;
     int num_transactions = 20; // Total number of transactions
 
     // DUT with parameterized width
-    alu #(WIDTH = ALU_WIDTH) dut_alu (
-        .in_A, .in_B, .c_in, .op,
+    logic[`ALU_WIDTH-1:0] in_A, in_B, out;
+    logic c_in, c_out, f_zero, f_negative, f_overflow, f_parity;
+    opcode op;
+    alu #(.WIDTH(`ALU_WIDTH)) dut_alu (
+        .in_A(in_A), .in_B(in_B), .c_in(c_in), .op(op),
 
-        .out, .c_out, .f_zero, .f_negative, .f_overflow, .f_parity
+        .out(out), .c_out(c_out), .f_zero(f_zero), .f_negative(f_negative), 
+        .f_overflow(f_overflow), .f_parity(f_parity)
     );
 
     initial begin
@@ -66,7 +78,7 @@ module tb_alu;
 
         // Test for command line arguments
         if($test$plusargs("verbose")) begin
-            $display("Verbose logging enabled", alu_width);
+            $display("Verbose logging enabled");
             verbose = 1;
         end
         if ($value$plusargs("wait_time=%0d", wait_time)) begin
@@ -75,67 +87,64 @@ module tb_alu;
         if ($value$plusargs("num_transactions=%0d", num_transactions)) begin
             $display("Using custom num_transactions = %0d", num_transactions);
         end
-        
-        // Initialize transaction tracker
-        num_correct = 0;
-        num_total = 0;
     end
 
     initial begin
+        alu_transaction #(.WIDTH(`ALU_WIDTH)) transaction;
+        int in_A_int, in_B_int, expected_out, result;
+        bit c_in_bit, expected_c_out, expected_f_zero, expected_f_negative;
+        bit expected_f_overflow, expected_f_parity;
+
+        // Initialize transaction tracker
+        num_correct = 0;
+        num_total = 0;
+
         $display("T=%0t Starting...", $time);
-
         // Generate transactions in loop
-        alu_transaction #(.WIDTH (ALU_WIDTH)) transaction;
-        for (int i = 0; i < num_transactions; i++) {
+        for (int i = 0; i < num_transactions; i++) begin
             transaction = new; // Create new transaction
-            transaction.randomize(); // Randomize stimuli
+            transaction.new_random_inputs(); // Randomize stimuli
             if (verbose) $display("T=%0t Created transaction %0d/%0d", $time, i+1, num_transactions);
-
+ 
             // Calculate expected outputs
-            int expected_out;
-            bit expected_c_out, expected_f_zero, expected_f_negative;
-            bit expected_f_overflow, expected_f_parity;
-
             // Cast transaction elements to appropriate types, just in case
-            int in_A = signed'(transaction.in_A);
-            int in_B = signed'(transaction.in_B);
-            bit c_in = bit'(transaction.c_in);
-            opcode op = transaction.op;
+            in_A_int = signed'(transaction.in_A);
+            in_B_int = signed'(transaction.in_B);
+            c_in_bit = bit'(transaction.c_in);
 
             // set expected_out based on op
-            int result;
-            case (op)
-                PASSTHROUGH     : result = in_A;
-                ADD             : result = in_A + in_B;
-                ADD_WITH_CIN    : result = in_A + in_B + c_in;
-                SUBTRACT        : result = in_A - in_B;
-                SUB_WITH_CIN    : result = in_A - in_B - ~c_in;
-                TWOS_COMPLEMENT : result = -1 * tx.in_A;
-                INCREMENT       : result = in_A + 1;
-                DECREMENT       : result = in_A - 1;
-                BIT_AND         : result = in_A & in_B;
-                BIT_OR          : result = in_A | in_B;
-                BIT_XOR         : result = in_A ^ in_B;
-                BIT_NOT         : result = ~in_A;
-                ASR             : result = in_A >>> 1;
-                LSR             : result = in_A >> 1;
-                SHIFT_LEFT      : result = in_A << 1;
-                ROTATE_LEFT     : result = in_A >> 1 + (in_A % 2) << (ALU_WIDTH-1);
+            case (transaction.op)
+                PASSTHROUGH     : result = in_A_int;
+                ADD             : result = in_A_int + in_B_int;
+                ADD_WITH_CIN    : result = in_A_int + in_B_int + c_in_bit;
+                SUBTRACT        : result = in_A_int - in_B_int;
+                SUB_WITH_CIN    : result = in_A_int - in_B_int - ~c_in_bit;
+                TWOS_COMPLEMENT : result = -1 * in_A_int;
+                INCREMENT       : result = in_A_int + 1;
+                DECREMENT       : result = in_A_int - 1;
+                BIT_AND         : result = in_A_int & in_B_int;
+                BIT_OR          : result = in_A_int | in_B_int;
+                BIT_XOR         : result = in_A_int ^ in_B_int;
+                BIT_NOT         : result = ~in_A_int;
+                ASR             : result = in_A_int >>> 1;
+                LSR             : result = in_A_int >> 1;
+                SHIFT_LEFT      : result = in_A_int << 1;
+                ROTATE_LEFT     : result = (in_A_int << 1) + (in_A_int >> (`ALU_WIDTH-1));
             endcase
-            expected_out = (ALU_WIDTH)'(result);
+            expected_out = (`ALU_WIDTH)'(result);
 
             // set expected c_out based on op
-            case (op)
-                ADD             : expected_c_out = result >> ALU_WIDTH;
-                ADD_WITH_CIN    : expected_c_out = result >> ALU_WIDTH;
-                SUBTRACT        : expected_c_out = result >> ALU_WIDTH;
-                SUB_WITH_CIN    : expected_c_out = result >> ALU_WIDTH;
-                TWOS_COMPLEMENT : expected_c_out = result >> ALU_WIDTH;
-                INCREMENT       : expected_c_out = result >> ALU_WIDTH;
-                DECREMENT       : expected_c_out = result >> ALU_WIDTH;
-                ASR             : expected_c_out = in_A[0]; // Rotated bit off
-                LSR             : expected_c_out = in_A[0];
-                SHIFT_LEFT      : expected_c_out = in_A[ALU_WIDTH-1];
+            case (transaction.op)
+                ADD             : expected_c_out = result >> `ALU_WIDTH;
+                ADD_WITH_CIN    : expected_c_out = result >> `ALU_WIDTH;
+                SUBTRACT        : expected_c_out = result >> `ALU_WIDTH;
+                SUB_WITH_CIN    : expected_c_out = result >> `ALU_WIDTH;
+                TWOS_COMPLEMENT : expected_c_out = result >> `ALU_WIDTH;
+                INCREMENT       : expected_c_out = result >> `ALU_WIDTH;
+                DECREMENT       : expected_c_out = result >> `ALU_WIDTH;
+                ASR             : expected_c_out = in_A_int[0]; // Rotated bit off
+                LSR             : expected_c_out = in_A_int[0];
+                SHIFT_LEFT      : expected_c_out = in_A_int[`ALU_WIDTH-1];
                 default         : expected_c_out = 0;
             endcase
 
@@ -143,40 +152,41 @@ module tb_alu;
             expected_f_zero = (expected_out == 0);
             expected_f_negative = (expected_out < 0);
             // A and B have same sign + A and out have different signs
-            expected_f_overflow = ((in_A<0) == (in_B<0)) && ((in_A<0) != (expected_out<0));
+            expected_f_overflow = ((in_A_int<0) == (in_B_int<0)) && ((in_A_int<0) != (expected_out<0));
+            expected_f_parity = ^expected_out;
 
             // Drive inputs to DUT
-            dut_alu.in_A = transaction.in_A;
-            dut_alu.in_B = transaction.in_B;
-            dut_alu.c_in = transaction.c_in;
-            dut_alu.op = transaction.op;
-            if (verbose) $display("T=%0t Sent transaction %s", $time, tx.inputs_to_string());
+            in_A = transaction.in_A;
+            in_B = transaction.in_B;
+            c_in = transaction.c_in;
+            op = transaction.op;
+            if (verbose) $display("T=%0t Sent transaction %s", $time, transaction.inputs_to_string());
+
+            #(wait_time); // Wait for wait_time ns until next transaction
 
             // Capture outputs from DUT
-            transaction.out = dut_alu.out;
-            transaction.c_out = dut_alu.c_out;
-            transaction.f_zero = dut_alu.f_zero;
-            transaction.f_negative = dut_alu.f_negative;
-            transaction.f_overflow = dut_alu.f_overflow;
-            transaction.f_parity = dut_alu.f_parity;
+            transaction.out = out;
+            transaction.c_out = c_out;
+            transaction.f_zero = f_zero;
+            transaction.f_negative = f_negative;
+            transaction.f_overflow = f_overflow;
+            transaction.f_parity = f_parity;
 
             // Check that all outputs match expected values
             if (transaction.out == expected_out && transaction.c_out == expected_c_out
                 && transaction.f_zero == expected_f_zero && transaction.f_negative == expected_f_negative
                 && transaction.f_overflow == expected_f_overflow && transaction.f_parity == expected_f_parity) begin
-                if (verbose) $display("T=%0t Passed %s", $time, tx.result_to_string());
+                if (verbose) $display("T=%0t Passed %s", $time, transaction.result_to_string());
                 num_correct += 1;
             end
             else begin // Fail if they don't match
-                $display("T=%0t FAILED %s\n    EXPECTED out=%0d c_out=%1b f_zero=%1b f_negative=%1b 
-                    f_overflow=%1b f_parity=%1b", $time, tx.result_to_string(), expected_out, expected_c_out,
+                $display("T=%0t FAILED %s\n    EXPECTED out=%0d c_out=%1b f_zero=%1b f_negative=%1b f_overflow=%1b f_parity=%1b", 
+                    $time, transaction.result_to_string(), expected_out, expected_c_out,
                     expected_f_zero,expected_f_negative, expected_f_overflow, expected_f_parity);
             end
 
             num_total += 1; // Increment transaction counter
-
-            #(wait_time); // Wait for wait_time ns until next transaction
-        }
+        end
 
         // Print final result
         $display("T=%0t FINAL: %0d/%0d correct", $time, num_correct, num_total);

643a17a062ca4a665d329a9ef297d0a900267b4d
Brandon Wang <brandonwangHSV@gmail.com>
2026-01-26 10:01:07 -0800

    add localparams for bit positions

--- FPU/src/fpu_unpack.sv
+++ FPU/src/fpu_unpack.sv
@@ -6,7 +6,15 @@ module fpu_unpack #(
     EXPONENT_WIDTH = 11,
     SIGNIFICAND_WIDTH = 52
 ) (
-    ports
+    input 
 );
+
+localparam int packed_width = SIGN_WIDTH + EXPONENT_WIDTH + SIGNIFICAND_WIDTH; // total bitwidth of fp number
+localparam int significand_start = SIGNIFICAND_WIDTH - 1; // pos of first signicand bit
+localparam int significand_end = 0; // pos of last signicand bit
+localparam int exponent_start = significand_start + EXPONENT_WIDTH; // pos of first exponent bit
+localparam int exponent_end = significand_start + 1; // pos of last exponent bit
+localparam int sign_pos = exponent_start + 1; // pos of sign bit
+
     
 endmodule

8e9aa05ad05e0f7073a0124e8ef1678cb01cbee6
Brandon Wang <brandonwangHSV@gmail.com>
2026-01-26 11:24:19 -0800

    Implement fpu_pack

--- FPU/src/fpu_pack.sv
+++ FPU/src/fpu_pack.sv
@@ -5,7 +5,19 @@ module fpu_pack #(
     EXPONENT_WIDTH = 11,
     SIGNIFICAND_WIDTH = 52
 ) (
-    ports
+    // Unpacked input
+    input logic sign,
+    input logic[EXPONENT_WIDTH-1:0] exponent,
+    input logic[SIGNIFICAND_WIDTH:0] significand // with implied bit
+
+    // Packed output
+    output logic[packed_width:0] packed_fp,
 );
-    
+
+    localparam int packed_width = 1 + EXPONENT_WIDTH + SIGNIFICAND_WIDTH; // total bitwidth of fp number
+
+    always_comb begin : pack_logic
+        packed_fp = {sign, exponent, significand[SIGNIFICAND_WIDTH-1:0]}
+    end
+
 endmodule

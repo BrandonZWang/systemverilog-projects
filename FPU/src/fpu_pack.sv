290d731c74e294fe6718048cce4cae552c62bfe4
Brandon Wang <brandonwangHSV@gmail.com>
2026-01-26 11:40:57 -0800

    Fix ports

--- FPU/src/fpu_pack.sv
+++ FPU/src/fpu_pack.sv
@@ -8,10 +8,10 @@ module fpu_pack #(
     // Unpacked input
     input logic sign,
     input logic[EXPONENT_WIDTH-1:0] exponent,
-    input logic[SIGNIFICAND_WIDTH:0] significand // with implied bit
+    input logic[SIGNIFICAND_WIDTH:0] significand, // with implied bit
 
     // Packed output
-    output logic[packed_width-1:0] packed_fp,
+    output logic[packed_width-1:0] packed_fp
 );
 
     localparam int packed_width = 1 + EXPONENT_WIDTH + SIGNIFICAND_WIDTH; // total bitwidth of fp number
